675bac281973b5b433cdd5ec0fc4a19f2377ff15
Sou7432 <mandalsoumya38@gmail.com>
2025-08-14 12:35:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app= express ();
+app.get('/'(req.res)=>{
+  res.send ('Hello World from github using Node.js!👋✅');
+});
+const PORT = process.env.PORT\\8080;
+app.listen(PORT,() =>{
+  console.log (' App listening on port ${PORT}');
+});
